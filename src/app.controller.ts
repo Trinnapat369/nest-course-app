0c269c5a32cf24b28a88f3c4fc6478e32ccad06e
Trinnapat Naksan <omobily6578@gmail.com>
2025-02-14 10:10:23 +0700

    first commit”

---
+++ src/app.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/showname')//lo
+  showName(): string {
+    return this.appService.getName();
+  }
+  
+  @Get('/showInformation')//lo
+  showInfo(): string {
+    return this.appService.getInfo();
+  }
+  @Get('/showjson')
+  getJSON(){
+    return this.appService.getJson();
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'We love NestJS';
+  }
+  getName(): string {
+    return 'Trinnapat';
+  }
+
+  getInfo(): string {
+    return'Hello,I am Trinnapat Naksan,19 years old';
+  }
+
+  getJson(){
+    return {
+      name: 'Trinnapat',
+      lastName: 'Naksan',
+      age:19,
+    };
+  }
+}
